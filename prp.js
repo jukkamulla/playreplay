664f2060df49204116364cdd3959bc2a5819e8c5
jukkamulla <jukkamulla@gmail.com>
2015-02-11 20:48:47 +0100

    add simple game logic

--- prp.js
+++ prp.js
@@ -1,1 +1,17 @@
+var i = 1;
+var interval;
 
+function sequence() {
+    var sequenceElement = document.getElementById("sequence");
+    var sequenceParagraph = sequenceElement.firstChild;
+    sequenceParagraph.innerHTML = i.toString();
+    i++;
+    if (i === 11) {
+        clearInterval(interval);
+        i = 0;
+    }
+}
+
+function go(){
+    interval = setInterval(sequence, 1000);
+}
