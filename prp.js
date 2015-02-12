7b0ffa2584c706190cbee020c668d169ee4f798e
jukkamulla <jukkamulla@gmail.com>
2015-02-12 21:18:03 +0100

    add counter logic
    Add new elements for next task.

--- prp.js
+++ prp.js
@@ -1,17 +1,31 @@
 var i = 1;
+var j = 0;
 var interval;
+
+function setCounter() {
+    var counterElement = document.getElementById("counter");
+    var counterParagraph = counterElement.firstChild;
+    j++;
+    counterParagraph.innerHTML = j.toString();
+}
 
 function sequence() {
     var sequenceElement = document.getElementById("sequence");
     var sequenceParagraph = sequenceElement.firstChild;
     sequenceParagraph.innerHTML = i.toString();
     i++;
+
     if (i === 11) {
         clearInterval(interval);
+        setCounter();
         i = 0;
     }
 }
 
-function go(){
+function go() {
     interval = setInterval(sequence, 1000);
+}
+
+function checkResult() {
+    console.log('test');
 }
