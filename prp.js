13a1c9a5d9e2fd4ffcc5190026fb369c08812bc8
jukkamulla <jukkamulla@gmail.com>
2015-02-22 17:44:48 +0100

    fix round logic

--- prp.js
+++ prp.js
@@ -2,10 +2,69 @@ var timer = 0;
 var playerWins = 0;
 var rightValueCounter = 0;
 var sequenceTime = 5000;
+var maxRightValue = 2;
 var interval;
 var setNumber1;
 var setNumber2;
 
+
+function go() {
+    interval = setInterval(sequence, sequenceTime);
+    setRandomNumbers();
+}
+
+function setRandomNumbers() {
+    if (rightValueCounter >= maxRightValue){
+        return false;
+    }
+    setNumber1 = Math.floor((Math.random() * 99) + 1);
+    setNumber2 = Math.floor((Math.random() * 99) + 1);
+    displayNumber();
+}
+
+function displayNumber() {
+    var number1 = document.getElementById("number1");
+    var number2 = document.getElementById("number2");
+    number1.innerHTML = setNumber1.toString();
+    number2.innerHTML = setNumber2.toString();
+}
+
+function checkResult() {
+    if (interval) {
+        var resultElement = document.getElementById("result");
+        if (resultElement.value == setNumber1 + setNumber2) {
+            rightValueCounter++;
+            setHtmlValue("player", rightValueCounter);
+            resultElement.value = "";
+            setRandomNumbers();
+        }
+        isPlayerWins()
+    }
+}
+
+function isPlayerWins() {
+    if (rightValueCounter === maxRightValue) {
+        if (timer < 10) {
+            playerWins++;
+            setHtmlValue("counter", playerWins);
+        }
+        resetRound();
+    }
+}
+
+function resetRound() {
+    clearInterval(interval);
+    interval = false;
+    
+    timer = 0;
+    setNumber1 = 0;
+    setNumber2 = 0;
+    rightValueCounter = 0;
+
+    displayNumber();
+    setHtmlValue("sequence", timer);
+    setHtmlValue("player", rightValueCounter);
+}
 
 function setHtmlValue(id, value) {
     var element = document.getElementById(id).firstChild;
@@ -23,48 +82,9 @@ function sequence() {
     }
 }
 
-function setRandomNumbers() {
-    var number1 = document.getElementById("number1");
-    setNumber1 = Math.floor((Math.random() * 99) + 1);
-    var number2 = document.getElementById("number2");
-    setNumber2 = Math.floor((Math.random() * 99) + 1);
-    number1.innerHTML = setNumber1.toString();
-    number2.innerHTML = setNumber2.toString();
-}
-
-function go() {
-    interval = setInterval(sequence, sequenceTime);
-    setRandomNumbers();
-}
-
-function checkResult() {
-    var resultElement = document.getElementById("result");
-    if (resultElement.value == setNumber1 + setNumber2) {
-        rightValueCounter++;
-        setHtmlValue("player", rightValueCounter);
-        resultElement.value = "";
-        setRandomNumbers();
-    }
-    function isPlayerWins() {
-        if (rightValueCounter === 10) {
-            if (timer < 10) {
-                playerWins++;
-                setHtmlValue("counter", playerWins);
-            }
-            clearInterval(interval)
-            timer = 0;
-            setHtmlValue("sequence", timer);
-            rightValueCounter = 0;
-            setHtmlValue("player", rightValueCounter);
-        }
-    }
-    isPlayerWins()
-}
-
 function keyCode(event) {
     var key = event.which || event.keyCode;
     if (key === 13) {
-        console.log("press enter");
         checkResult();
     }
 }
